8411ef0f649dfc6807983b16b176b9df79d6ee24
Ian <ian.nancarrow@detroitlabs.com>
2017-04-18 15:43:31 -0400

    Made a more intricate battle system: Knight gets hit, has health, dragon attacks, knight can block damage, dragon deals random damage to knight, knight dies when hp is less than 1, all systems stop working when knight dies.

--- DragonGold.js
+++ DragonGold.js
@@ -29,83 +29,151 @@ var sleep = function(milliseconds) {
 	var goldGain = 1;
 	var winGame = false;
 	var gold = 0;
+	var hp = 100;
 	var health = document.getElementById('Health');
 	var defending = false;
+	var playerDied = false;
 
 	//Functions******************
 
 	playerDefend.addEventListener('mouseover', function(event) {
-		playerDefend.src = "images/knightd.png";
-		eWeapon.style.visibility = "hidden";
-		defending = true;
-		// sleep(2000);
-		// playerDefend.src = "images/knight.png"
-		// defending = false;
+		if (playerDied == false) {
+			playerDefend.src = "images/knightd.png";
+			eWeapon.style.visibility = "hidden";
+			defending = true;
+			// sleep(2000);
+			// playerDefend.src = "images/knight.png"
+			// defending = false;
+		}
 	})
 
 	playerDefend.addEventListener('mouseout', function(event) {
-		playerDefend.src = "images/knight.png";
-		eWeapon.style.visibility = "visible";
-		defending = false;
-		// sleep(2000);
-		// playerDefend.src = "images/knight.png"
-		// defending = false;
+		if (playerDied == false) {
+			playerDefend.src = "images/knight.png";
+			eWeapon.style.visibility = "visible";
+			defending = false;
+			// sleep(2000);
+			// playerDefend.src = "images/knight.png"
+			// defending = false;
+		}
 	})
 
 
 
 	playerGoldClick.addEventListener('click', function(event) {
-		if (winGame == false) {
-			gold += goldGain;
-			//innerHTML is the content of an element - this is basically taking the number of gold and displaying it in html.
-			goldValue.innerHTML = gold;
-		} else if (winGame == true) {
-			gold = 1000000;
-			goldValue.innerHTML = gold;
-			playerGoldClick.src = "images/boom.png";
+		if (playerDied == false) {
+			if (winGame == false) {
+				gold += goldGain;
+				//innerHTML is the content of an element - this is basically taking the number of gold and displaying it in html.
+				goldValue.innerHTML = gold;
+			} else if (winGame == true) {
+				gold = 1000000;
+				goldValue.innerHTML = gold;
+				playerGoldClick.src = "images/boom.png";
+			}
+		} else if (playerDied == true) {
+			goldGain = 0;
 		}
 	})
 
 	playerItem1Click.addEventListener('click', function(event) {
-		if (gold >= 20 && soldout1 == false) {
-			gold -= 20;
-			goldGain = 2;
-			playerItem1Click.src = "images/soldout.png";
-			goldValue.innerHTML = gold;
-			soldout1 = true;
-			eWeapon.src = "images/esword1.png";
+		if (playerDied == false) {
+			if (gold >= 20 && soldout1 == false) {
+				gold -= 20;
+				goldGain = 2;
+				playerItem1Click.src = "images/soldout.png";
+				goldValue.innerHTML = gold;
+				soldout1 = true;
+				eWeapon.src = "images/esword1.png";
+			}
 		}
 	})
 
 	playerItem2Click.addEventListener('click', function(event) {
-		if (gold >= 50 && soldout2 == false) {
-			gold -= 50;
-			goldGain = 5;
-			playerItem1Click.src = "images/soldout.png";
-			playerItem2Click.src = "images/soldout.png";
-			goldValue.innerHTML = gold;
-			soldout1 = true;
-			soldout2 = true;
-			eWeapon.src = "images/esword2.png";
+		if (playerDied == false) {
+			if (gold >= 50 && soldout2 == false) {
+				gold -= 50;
+				goldGain = 5;
+				playerItem1Click.src = "images/soldout.png";
+				playerItem2Click.src = "images/soldout.png";
+				goldValue.innerHTML = gold;
+				soldout1 = true;
+				soldout2 = true;
+				eWeapon.src = "images/esword2.png";
+			}
 		}
 	})
 
 	playerItem3Click.addEventListener('click', function(event) {
-		if (gold >= 100 && soldout3 == false) {
-			gold -= 100;
-			playerItem1Click.src = "images/soldout.png";
-			playerItem2Click.src = "images/soldout.png";
-			playerItem3Click.src = "images/soldout.png";
-			goldValue.innerHTML = gold;
-			soldout1 = true;
-			soldout2 = true;
-			soldout3 = true;
-			winGame = true;
-			eWeapon.src = "images/esword3.png";
+		if (playerDied == false) {
+			if (gold >= 100 && soldout3 == false) {
+				gold -= 100;
+				playerItem1Click.src = "images/soldout.png";
+				playerItem2Click.src = "images/soldout.png";
+				playerItem3Click.src = "images/soldout.png";
+				goldValue.innerHTML = gold;
+				soldout1 = true;
+				soldout2 = true;
+				soldout3 = true;
+				winGame = true;
+				eWeapon.src = "images/esword3.png";
+			}
 		}
 	})
 
+	function delayer() {
+		setTimeout(function(){ randomAttack(); }, 3000);
+	}
 
+	function randomAttack() {
+		if (hp >= 1) {
+			playerGoldClick.src = "images/dragon.png";
+			playerDefend.src = "images/knight.png";
+			eWeapon.style.visibility = "visible";
+			var ranNum = Math.floor((Math.random() * 5000) + 1000);
+		    setTimeout(function(){ attacking(); }, ranNum);
+		} else if (hp <= 0) {
+			hp = 0;
+			health.innerHTML = hp;
+			playerDied = true;
+			playerDefend.src = "images/RIP.png";
+			eWeapon.style.visibility = "hidden";
+		}
+	}
+
+	function attacking() {
+		if (winGame == true) {
+			
+		} else if (winGame == false) {
+			playerGoldClick.src = "images/dragona.png";
+			setTimeout(function(){ attackKnight(); }, 1000);
+		}
+
+	}
+
+	function attackKnight() {
+		//playerGoldClick.src = "images/dattack.png";
+		
+		if (defending == true) {
+			randomAttack();
+		} else if (defending == false) {
+			var ranDam = Math.floor((Math.random() * 50) + 5);
+			hp -= ranDam;
+			health.innerHTML = hp;
+			knightHit();
+		}
+	}
+
+	function knightHit() {
+		playerGoldClick.src = "images/dragon.png"
+		playerDefend.src = "images/knighthit.png"
+		eWeapon.style.visibility = "hidden";
+		setTimeout(function(){ randomAttack(); }, 1000);
+	}
+
+
+
+	delayer();
 
 
 
